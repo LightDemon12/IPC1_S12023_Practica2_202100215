b2f0ec791a2252c86a574c582940a6e62f65b7a6
Angel Guillermo de Jesús Pérez Jiménez <140100841+LightDemon12@users.noreply.github.com>
2023-10-05 17:16:51 -0600

    LightDemon12
    cambio hilos

--- src/Interfaz/VistaEntregaDePedidos.java
+++ src/Interfaz/VistaEntregaDePedidos.java
@@ -84,7 +84,7 @@ public class VistaEntregaDePedidos extends javax.swing.JFrame {
                     .addComponent(MOTOCICLETA1)
                     .addComponent(MOTOCICLETA3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(BARRERA, javax.swing.GroupLayout.PREFERRED_SIZE, 13, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(BARRERA, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
--- src/MOTOCICLETAS/Carrera.java
+++ src/MOTOCICLETAS/Carrera.java
@@ -5,41 +5,31 @@ import Interfaz.VistaEntregaDePedidos;
 import static java.lang.Math.random;
 import javax.swing.JLabel;
 
-
-public class Carrera  extends Thread{ 
-    private JLabel etiqueta; 
-    private VistaEntregaDePedidos moto; 
+public class Carrera extends Thread {
+    private JLabel etiqueta;
+    private VistaEntregaDePedidos moto;
 
     public Carrera(JLabel etiqueta, VistaEntregaDePedidos moto) {
         this.etiqueta = etiqueta;
         this.moto = moto;
     }
+
     @Override
-public void run() {
-    int moto1 =0 ;
-    int moto2 =0 ;
-    int moto3 =0 ;
-    try {
-        while (true) {
-            Thread.sleep((int) (Math.random() * 100));
-            moto1 =  moto.getMOTOCICLETA1().getLocation().x;
-            moto2 =  moto.getMOTOCICLETA2().getLocation().x;
-            moto3 =  moto.getMOTOCICLETA3().getLocation().x;
-            
-            
-            if (moto1 < moto.getLocation().x +125&& moto2 < moto.getLocation().x&&moto3 < moto.getLocation().x){
-            etiqueta.setLocation(etiqueta.getLocation().x+10, etiqueta.getLocation().y);
-            moto.repaint();
-        }else {
-                break;
+    public void run() {
+        try {
+            int distanciaDeseada = 20; // La distancia deseada antes de detenerse
+            while (etiqueta.getLocation().x < moto.getBARRERA().getLocation().x - distanciaDeseada) {
+                int velocidad = (int) (Math.random() * 100); // Velocidad aleatoria
+                Thread.sleep(velocidad);
+                etiqueta.setLocation(etiqueta.getLocation().x + 10, etiqueta.getLocation().y);
+                moto.repaint();
             }
+        } catch (InterruptedException e) {
+            System.out.println("Se produjo una excepción: " + e.getMessage());
         }
-        
-    } catch (InterruptedException e) {
-        System.out.println("Se produjo una excepción: " + e.getMessage());
-    }
-    if(etiqueta.getLocation().x>= moto.getBARRERA().getLocation().x){
-        System.out.println("completado");
+        if (etiqueta.getLocation().x >= moto.getBARRERA().getLocation().x - 50) {
+            System.out.println("Completado");
+        }
     }
 }
-}
+
